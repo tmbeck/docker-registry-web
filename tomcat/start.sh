c8ec1633cca610a321499c79766864c44b20eb50
Tim Beck <tmbeck@gmail.com>
2016-08-28 21:05:09 -0700

    Fixup start.sh so that SSL actually gets turned.

--- tomcat/start.sh
+++ tomcat/start.sh
@@ -6,16 +6,20 @@ CONTEXT_PATH=$(yml.pl /conf/config.yml registry.context_path)
 CATALINA_OPTS="$CATALINA_OPTS -Dcontext.path=${REGISTRY_CONTEXT_PATH:-$CONTEXT_PATH}"
 echo CATALINA_OPTS: $CATALINA_OPTS
 
-if [[ -d $CERT_DIR && -z "$KEYSTORE_PASS" ]]
+if [[ -d $CERT_DIR && ! -z "$KEYSTORE_PASS" ]]
 then
 	if [[ ! -f ${CERT_DIR}/cert_and_key.p12 ]]
 	then
 		openssl pkcs12 -export -in ${CERT_DIR}/fullchain.pem -inkey ${CERT_DIR}/privkey.pem -out ${CERT_DIR}/cert_and_key.p12 -name tomcat -CAfile ${CERT_DIR}/chain.pem -caname root -password pass:"$KEYSTORE_PASS"
 	fi
 
-	keytool -importkeystore -srcstorepass "$KEYSTORE_PASS" -deststorepass changeit -destkeypass changeit -srckeystore ${CERT_DIR}/cert_and_key.p12 -srcstoretype PKCS12 -alias tomcat -keystore $KEYSTORE
-	keytool -import -trustcacerts -alias root -deststorepass changeit -file ${CERT_DIR}/chain.pem -noprompt -keystore $KEYSTORE
-	sed -i 's/SSLEnabled="false"/SSLEnabled="true"/'
+	if [[ ! -f ${KEYSTORE} ]]
+	then
+		keytool -importkeystore -srcstorepass "$KEYSTORE_PASS" -deststorepass changeit -destkeypass changeit -srckeystore ${CERT_DIR}/cert_and_key.p12 -srcstoretype PKCS12 -alias tomcat -keystore $KEYSTORE
+		keytool -import -trustcacerts -alias root -deststorepass changeit -file ${CERT_DIR}/chain.pem -noprompt -keystore $KEYSTORE
+	fi
+	
+	sed -i 's/SSLEnabled="false"/SSLEnabled="true"/' $CATALINA_BASE/conf/server.xml
 fi
 
 exec catalina.sh run
